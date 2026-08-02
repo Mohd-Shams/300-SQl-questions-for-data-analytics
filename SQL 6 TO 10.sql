9bd32d60be4555fdc85881ead144cb76265a5df0
Mohd Shams <mohdshamsm20@gmail.com>
2026-08-02 14:41:44 +0530

    Question 6 to 10

--- SQL 6 TO 10.sql
+++ SQL 6 TO 10.sql
@@ -12,21 +12,40 @@ SELECT ROUND(AVG(total_amount),2)
 FROM ecommerce_merged_dataset;
 
 -- 8.Which customers placed more than five orders?
-SELECT customer_id,customer_name, COUNT(quantity)AS order_count
+SELECT
+    customer_id,
+    customer_name,
+    COUNT(DISTINCT order_id) AS order_count
 FROM ecommerce_merged_dataset
-GROUP BY customer_id ,customer_name
-HAVING order_count > 5
+GROUP BY
+    customer_id,
+    customer_name
+HAVING COUNT(DISTINCT order_id) > 5
 ORDER BY order_count DESC;
 
 
 -- 9.High-Value Customers
 
 -- Identify customers whose spending is above the average customer spending.
-SELECT customer_id,customer_name,SUM(total_amount)
+SELECT
+    customer_id,
+    customer_name,
+    SUM(total_amount) AS total_spending
 FROM ecommerce_merged_dataset
-WHERE total_amount > (SELECT ROUND(AVG(total_amount),2)
-FROM ecommerce_merged_dataset)
-GROUP BY customer_id ,customer_name;
+GROUP BY
+    customer_id,
+    customer_name
+HAVING SUM(total_amount) >
+(
+    SELECT AVG(customer_total)
+    FROM
+    (
+        SELECT SUM(total_amount) AS customer_total
+        FROM ecommerce_merged_dataset
+        GROUP BY customer_id
+    ) AS customer_spending
+)
+ORDER BY total_spending DESC;
 
 -- 10. Repeat Purchase Rate
 
