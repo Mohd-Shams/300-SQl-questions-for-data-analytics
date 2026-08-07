a299308044c2d48fb81b57e39f5048e083ed2dc2
Mohd Shams <mohdshamsm20@gmail.com>
2026-08-07 16:27:21 +0530

    Questions 31 to 35

--- SQL 31 TO 35.sql
+++ SQL 31 TO 35.sql
@@ -3,8 +3,54 @@
 
 -- The regional sales director wants to identify the five cities generating the highest revenue to 
 -- prioritize future investments and marketing campaigns.
-SELECT region,ROUND(SUM(total_amount),2) AS total_revenue
+SELECT city,ROUND(SUM(total_amount),2) AS total_revenue
 FROM data3
-GROUP BY region
+GROUP BY city
 ORDER BY total_revenue DESC
 LIMIT 5;
+
+-- 32. Best Performing Sales Representative in Every Region
+-- Business Problem
+
+-- Management wants to reward the best-performing salesperson in each region based on total revenue generated.
+
+WITH sales_person AS(SELECT region ,sales_rep ,
+		ROUND(SUM(total_amount),2) AS total_revenue
+FROM data3
+GROUP BY region,sales_rep),
+
+ ranking AS (SELECT region,sales_rep,total_revenue,
+			DENSE_RANK() OVER (PARTITION BY region
+            ORDER BY total_revenue DESC) AS RK
+            FROM sales_person)
+            
+SELECT region,sales_rep,total_revenue,RK
+FROM RANKING
+WHERE RK = 1;
+
+
+
+-- 33. Average Order Value by Payment Method
+-- Business Problem
+
+-- The finance department wants to understand which payment method is associated with the highest average order value.
+
+SELECT payment_method ,ROUND(AVG(total_amount),2) AS avg_value FROM data3
+GROUP BY payment_method
+ORDER BY avg_value DESC
+LIMIT 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
